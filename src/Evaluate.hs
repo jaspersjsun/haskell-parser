dc384507d46a7871ddb15ad7cd40d8a7c403588c
Shijie Sun <septimus145@gmail.com>
2017-01-15 19:24:42 +0800

    Add memory to store the context

--- src/Evaluate.hs
+++ src/Evaluate.hs
@@ -4,51 +4,60 @@
 
 module Evaluate where
 
-import Lib (Expr(..), Val(..))
-eval :: Expr -> Val
+import Lib (Expr(..), Val(..), Mem)
+import qualified Data.Map as Map
+import Data.Maybe (isNothing, fromJust)
+eval :: Mem -> Expr -> Val
 
 -- | eval bool expression
-eval FalseLit  = BoolVal False
-eval TrueLit   = BoolVal True
-eval (Not p)   = BoolVal $ not $ evalBool p
-eval (And p q) = BoolVal $ evalBool p && evalBool q
-eval (Or p q)  = BoolVal $ evalBool p || evalBool q
+eval _ FalseLit  = BoolVal False
+eval _ TrueLit   = BoolVal True
+eval m (Not p)   = BoolVal $ not $ evalBool m p
+eval m (And p q) = BoolVal $ evalBool m p && evalBool m q
+eval m (Or p q)  = BoolVal $ evalBool m p || evalBool m q
 
 -- | eval floating-point expression
-eval (NumLit x)  = NumVal x
-eval (Add p q)   = NumVal $ evalNum p + evalNum q
-eval (Minus p q) = NumVal $ evalNum p - evalNum q
-eval (Mult p q)  = NumVal $ evalNum p * evalNum q
-eval (Div p q)   = NumVal $ evalNum p / evalNum q
+eval _ (NumLit x)  = NumVal x
+eval m (Add p q)   = NumVal $ evalNum m p + evalNum m q
+eval m (Minus p q) = NumVal $ evalNum m p - evalNum m q
+eval m (Mult p q)  = NumVal $ evalNum m p * evalNum m q
+eval m (Div p q)   = NumVal $ evalNum m p / evalNum m q
 
 -- | eval floating-point comparison
-eval (Eq p q)        = BoolVal $ evalNum p == evalNum q
-eval (Less p q)      = BoolVal $ evalNum p < evalNum q
-eval (LessEq p q)    = BoolVal $ evalNum p <= evalNum q
-eval (Greater p q)   = BoolVal $ evalNum p > evalNum q
-eval (GreaterEq p q) = BoolVal $ evalNum p >= evalNum q
+eval m (Eq p q)        = BoolVal $ evalNum m p == evalNum m q
+eval m (Less p q)      = BoolVal $ evalNum m p < evalNum m q
+eval m (LessEq p q)    = BoolVal $ evalNum m p <= evalNum m q
+eval m (Greater p q)   = BoolVal $ evalNum m p > evalNum m q
+eval m (GreaterEq p q) = BoolVal $ evalNum m p >= evalNum m q
 
 -- | eval list and char
-eval Nil              = NilVal
-eval (CharLit c)      = CharVal c
-eval (Cons p q)       = ConsVal (eval p) (eval q)
-eval (Car (Cons p q)) = eval p
-eval (Cdr (Cons p q)) = eval q
+eval _ Nil              = NilVal
+eval _ (CharLit c)      = CharVal c
+eval m (Cons p q)       = ConsVal (eval m p) (eval m q)
+eval m (Car (Cons p q)) = eval m p
+eval m (Cdr (Cons p q)) = eval m q
+
+-- | eval variable
+eval m (VarRef v) = 
+    if isNothing var
+        then error $ "lookup failed! no variable named " ++ v
+        else fromJust var
+    where var = Map.lookup v m
 
 fromBoolVal :: Val -> Bool
 fromBoolVal (BoolVal b) = b
-fromBoolVal _ = errorWithoutStackTrace "Evaluate.fromBoolVal: Not BoolResult"
+fromBoolVal _ = errorWithoutStackTrace "Evaluate.fromBoolVal: Not BoolVal"
 
-evalBool :: Expr -> Bool
-evalBool = fromBoolVal . eval
+evalBool :: Mem -> Expr -> Bool
+evalBool m e = fromBoolVal $ eval m e
 
 fromNumVal :: Val -> Double
 fromNumVal (NumVal b) = b
-fromNumVal _ = errorWithoutStackTrace "Evaluate.fromNumVal: Not NumResult"
+fromNumVal _ = errorWithoutStackTrace "Evaluate.fromNumVal: Not NumVal"
 
-evalNum :: Expr -> Double
-evalNum = fromNumVal . eval
+evalNum :: Mem -> Expr -> Double
+evalNum m e = fromNumVal $ eval m e
 
-evalWithErrorThrowing :: Either String Expr -> String
-evalWithErrorThrowing (Left errStr) = "not a valid bool expr: " ++ errStr
-evalWithErrorThrowing (Right expr) = show $ eval expr
+evalWithErrorThrowing :: Mem -> Either String Expr -> String
+evalWithErrorThrowing _ (Left errStr) = "not a valid bool expr: " ++ errStr
+evalWithErrorThrowing m (Right expr) = show $ eval m expr
--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -4,7 +4,7 @@ module Evaluator where
 
 import qualified Data.Map as Map
 import Lib
-import Evaluate (eval, fromBoolVal)
+import Evaluate (evalBool, eval)
 
 -- Evaluation function
 -- Given an initial memory, execute program and return the memory afterwards
@@ -12,15 +12,15 @@ evalProg :: Prog -> Mem -> Mem
 evalProg (StmtList (p:ps)) m = evalProg (StmtList ps) m' where
     m' = evalProg p m
 
-evalProg (VarSet var expr) m = Map.insert var (eval expr) m
+evalProg (VarSet var expr) m = Map.insert var (eval m expr) m
 
 evalProg (If expr stmt1 stmt2) m =
-    if (fromBoolVal . eval) expr
+    if evalBool m expr
         then evalProg stmt1 m
         else evalProg stmt2 m
 
 evalProg (While expr stmt) m =
-    if (fromBoolVal . eval) expr
+    if evalBool m expr
         then if stmt /= Skip
             then let m' = evalProg stmt m
                 in evalProg (While expr stmt) m'
